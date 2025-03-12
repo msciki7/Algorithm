c44d98a4de08a7bdee2ca542a7404c7336e4fb1e
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-12 23:04:43 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+
+int main(void) {
+
+	printf("Hello World!");
+	
+
+	return 0;
+}
