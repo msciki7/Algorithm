aa05927682d38a1cd300cdc0785ab4b569b1ceb0
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-19 00:28:00 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+int main(void) {
+	
+	int a, b;
+	
+	scanf("%d %d", &a, &b);
+
+	printf("%d", a * b);
+
+	return 0;
+}
