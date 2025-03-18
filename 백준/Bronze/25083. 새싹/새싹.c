4338906aaa0b32a12767bcec41933f064a9cb573
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-19 00:33:39 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+int main(void) {
+	
+	printf("         ,%c\'\"%d\n", 'r', 7); // (a)
+	printf("%c`-_   ,\'  ,/\n", 'r');
+	printf(" \\. \". %c_%c\'\n", 'L', 'r'); // 
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+
+	return 0;
+}
