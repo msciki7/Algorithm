b944405044f6a26d4479a50bc7c18237a4fdc229
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-19 00:30:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+int main(void) {
+	
+	char ch;
+
+	scanf("%c", &ch);
+	printf("%d", (int)ch);
+
+	return 0;
+}
