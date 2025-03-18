2d4da5af8da36aaf58ffcbdd9ab5560a6cbc4e3b
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-19 00:21:46 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+int main(void) {
+	
+	int a, b;
+	
+	while (1) {
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0)
+			break;
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
