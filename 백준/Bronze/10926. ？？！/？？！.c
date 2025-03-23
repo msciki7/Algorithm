24afb5179502cab16f9d13dcb918e3dbca09effd
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-23 21:48:48 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+int main(void) {
+
+    char id[100];
+
+    scanf("%s", &id);
+    printf("%s??!", id);
+
+    return 0;
+    
+}
