bb72884acdcf9ea683881e2b4b4c52e7adde6ade
msciki7 <112370790+msciki7@users.noreply.github.com>
2025-03-26 23:43:17 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable:4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define SEC_PER_MINUTE 60
+
+
+int main(void) {
+
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군");
+
+	return 0;
+}
