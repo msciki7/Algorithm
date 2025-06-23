89b10c039a0d1f97051708864adb4c53c918c80f
sohee <112370790+msciki7@users.noreply.github.com>
2025-06-23 15:22:57 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	while (1) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		if (a == 0 && b == 0) {
+			break;
+		}
+
+		if (a > b) {
+			printf("Yes\n");
+		}
+		else {
+			printf("No\n");
+		}
+
+	}
+
+	return 0;
+}
