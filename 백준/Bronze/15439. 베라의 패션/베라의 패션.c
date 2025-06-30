529e7a585cd8446d72c51ae26877093938ffec92
sohee <112370790+msciki7@users.noreply.github.com>
2025-06-30 13:28:19 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    printf("%d", n * (n - 1));
+
+    return 0;
+}
+
