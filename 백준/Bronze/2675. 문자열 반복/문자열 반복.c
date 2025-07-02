a58cecee752f3b568ee04906a923fc7477f5e21a
sohee <112370790+msciki7@users.noreply.github.com>
2025-07-02 13:30:49 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int r;
+    char s[21];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s", &r, s);
+
+        for (int j = 0; j < strlen(s); j++) {
+            for (int k = 0; k < r; k++) {
+                printf("%c", s[j]);
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
