153affd3757e8a95f91cd16bd3eca0cb8b236264
sohee <112370790+msciki7@users.noreply.github.com>
2025-07-13 11:06:00 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(void) {
+
+	int a;
+	int b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("%d", a * b);
+
+	return 0;
+}
