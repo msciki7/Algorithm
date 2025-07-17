d9684a185122e734eaa633a65f3ec9719cfde0da
sohee <112370790+msciki7@users.noreply.github.com>
2025-07-17 14:22:23 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(void) {
+
+	char arr[5][15] = { NULL };
+
+	for (int i = 0; i< 5; i++) {
+		scanf("%s", arr[i]);
+	}
+
+	for (int j = 0; j < 15; j++) {
+		for (int i = 0; i < 5; i++) {
+			if (arr[i][j] != NULL) {
+				printf("%c", arr[i][j]);
+			}
+		}
+	}
+
+	return 0;
+}
