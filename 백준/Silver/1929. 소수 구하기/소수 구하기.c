26d45c56d52fcf1f0a6376ac65c4add778362bf4
sohee <112370790+msciki7@users.noreply.github.com>
2025-07-18 11:39:01 +0900

    [Silver III] Title: 소수 구하기, Time: 40 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(void) {
+
+	int m, n;
+	scanf("%d %d", &m, &n);
+
+	int arr[1000001];
+
+	for (int i = 2; i < 1000001; i++) {
+		arr[i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = i * 2; j <= n; j += i) {
+			arr[j] = 0;
+		}
+	}
+
+	for (int i = m; i <= n; i++) {
+		if (arr[i] == 1) {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
