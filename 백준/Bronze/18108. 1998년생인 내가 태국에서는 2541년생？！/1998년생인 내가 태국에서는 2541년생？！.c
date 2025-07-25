d47470e688ad0512d9598a233dc400e9f88ed764
sohee <112370790+msciki7@users.noreply.github.com>
2025-07-25 14:51:56 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    int y;                    // 1. 정수 변수 y 선언
+    scanf("%d", &y);          // 2. 정수를 입력받아서 변수 y에 저장
+    printf("%d", y - 543);    // 3. y-543의 결과값 출력
+
+    return 0;
+}
