45b8a1fc5bdf039309e403ef3d44be6c9da10ca3
sohee <112370790+msciki7@users.noreply.github.com>
2025-07-26 11:16:11 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(void) {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < n - i; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < 2*i - 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+
+}
